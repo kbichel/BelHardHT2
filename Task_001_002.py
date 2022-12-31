5b8ed4e2b6e30eab0a9709174116dd4765cbe256
kbichel <k.bichel@protonmail.com>
2022-12-31 18:04:21 +0000

    Task_001_002.py

---
+++ Task_001_002.py
@@ -0,0 +1,5 @@
+a = 7
+b = 'hello'
+c = 13.4
+print(f"parameter a = {a}\nparameter b = {b}\nparameter c = {c}\n")
+print(f"parameter {a} data type and identificator are:\n {type(a)}\n {id(a)}\nparameter {b} data type and identificator are:\n {type(b)}\n {id(b)}\nparameter {c} data type and identificator are:\n {type(c)}\n {id(c)}")
